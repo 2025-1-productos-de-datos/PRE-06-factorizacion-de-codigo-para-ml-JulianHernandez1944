bcf5ea96b31ef90c8e0183402a9d91a34fc967d3
JulianHernandez1944 <julhernandezar@unal.edu.co>
2025-05-24 08:48:41 -0500

    Factorizado

--- homework/src/_internals/save_model_if_better.py
+++ homework/src/_internals/save_model_if_better.py
@@ -6,10 +6,7 @@ from homework.src._internals.save_model import save_model
 
 
 def save_model_if_better(model, x_test, y_test, save_path="models/estimator.pkl"):
-    """
-    Guarda el modelo si es mejor que el modelo guardado anteriormente.
-    """
-    # Cargar el modelo guardado anteriormente
+    """Save the model if it performs better than the existing one."""
     best_model = None
     if os.path.exists(save_path):
         with open(save_path, "rb") as file:
